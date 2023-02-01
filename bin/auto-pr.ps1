dc0ff721093c32c8649757bb083b1922a71390fb
CoolingTool <62089272+CoolingTool@users.noreply.github.com>
2023-01-31 22:09:18 -0600

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "CoolingTool/scoop-lua:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
